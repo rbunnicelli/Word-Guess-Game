a0dcc17c4900780319f53b7f0314a4cfdc698561
Machoman <rbunnicelli2010@yahoo.com>
2018-06-29 02:28:32 -0400

    assets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
-var characters = ["Tommy", "Chuckie", "Phil", "Lil", "Angelica", "Reptar", "Arnold", "Gerald", "Helga", "Harold", "Grandpa", "Phoebe", "Eugene", "Stinky", "Spongebob", "Patrick", "Squidward", "Mr Krabs", "Plankton", "Sandy", "Gary", "Mermaid Man", "Barnacle Boy", "Otto", "Reggie", "Twister", "Squid", "Tito", "Raymundo"];
+var characters = ["tommy", "chuckie", "phil", "lil", "angelica", "reptar", "arnold", "gerald", "helga", "harold", "grandpa", "phoebe", "eugene", "stinky", "spongebob", "patrick", "squidward", "mr krabs", "plankton", "sandy", "gary", "mermaid man", "barnacle boy", "otto", "reggie", "twister", "squid", "tito", "raymundo"];
 var guessesLeft = 5;
 var randWord = '';
 var underScores = [];
 var userGuess = [];
 var wrongLetter = [];
 
-var placholder = document.getElementById('placeholders');
-var guessesLeft = document.getElementById('guesses-left');
-var guessedLetters = document.getElementById('guessed-letters');
+var guessesLeft = document.getElementById("guesses-left");
+var userGuess = document.getElementById("guessed-letters");
 
 //new game
 function startGame() {
 
 //picks random word from characters array
 randWord = characters[Math.floor(Math.random() * characters.length)];
+console.log(randWord);
+
 //Creates underscore for every letter of the random word chosen
 for(var i = 0; i < randWord.length; i++) {
     underScores.push('_');
@@ -27,27 +28,29 @@ guessesLeft = 5;
 
 // Connects with HTML element, guesses-left and prints the amount of guessesLeft from javascript var
 document.getElementById('guesses-left').textContent = guessesLeft;
+
 }
+//-----------------------------------------------------------------------------------------------------
 
 document.onkeyup = function(event) {
-    userGuess = event.key;
-//Checks to see if letter the user presses is in the randWord selected
-    if(randWord.indexOf(userGuess) > -1)
-    { //if letter in random word matches the user guess then fill underscore with that letter
-       for(var i = 0; i < randWord.length; i++) {
-        if(randWord[i] == userGuess) {
-            underScores[i] = userGuess;
+    var userGuess = event.key;
+
+    if(randWord.indexOf(userGuess) > -1) {
+        for(var i = 0; i < randWord.length; i++) {
+            if(randWord[i] == userGuess) {
+                underScores[i] = userGuess;
+            }
         }
-    } //if the letter is not an exact match then guesses left subtracts by 1
- }  else {
+    } else {
         wrongLetter.push(userGuess);
         guessesLeft--;
     }
+    document.getElementById('underscores').textContent = underScores.join(" ");
+    document.getElementById('guesses-left').textContent = guessesLeft;
 }
 
-
-
 startGame();
+
 
 
 
